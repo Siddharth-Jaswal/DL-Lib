53e9a0e53cc838e96403325732a808009217e8be
Siddharth Jaswal <sidatwork123@gmail.com>
2026-02-07 22:04:55 +0530

    added more fns to activation function

--- activation_function.py
+++ activation_function.py
@@ -15,3 +15,27 @@ def tanh(x):
 def dtanh(x):
     t = np.tanh(x)
     return 1 - t**2
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+def dReLU(x):
+    return (x > 0).astype(float)
+
+def leaky_ReLU(x,a=0.01):
+    arr = []
+    for xi in x:
+        if xi > 0:
+            arr.append(xi)
+        else:
+            arr.append(a*xi)
+    return np.array(arr)
+
+def dleaky_ReLU(x, a=0.01):
+    arr = []
+    for xi in x:
+        if xi > 0:
+            arr.append(1)
+        else:
+            arr.append(a)
+    return np.array(arr)
