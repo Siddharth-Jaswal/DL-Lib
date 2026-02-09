bf49c585b0cc60dbc7ab888eb47b2ec77007b78a
Siddharth Jaswal <sidatwork123@gmail.com>
2026-02-09 10:59:10 +0530

    added a utility => pnorm()

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+pi = 3.1415
 
 def seq(start,end,step):
     x = []
@@ -7,3 +8,7 @@ def seq(start,end,step):
         x.append(temp)
         temp += step  
     return np.array(x)
+
+def pnorm(x, mu, sigma):
+    return (1 / (np.sqrt(2 * np.pi) * sigma)) * \
+           np.exp(-0.5 * ((x - mu) / sigma) ** 2)
